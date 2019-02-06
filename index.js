ce9700d89b44ada09d31c908650018bb80f56293
dandradec <dandradec98@gmail.com>
2019-02-06 14:12:12 -0500

    Testing new setup for cors

--- index.js
+++ index.js
@@ -252,13 +252,7 @@ app.get("/Artist/:id/:name/Albums", (req, res) => {
         });
 });
 
-//app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
+app.use(cors( {credentials: true, origin: true} ));
 
 console.log(`Myzuka API running on port ${port}`);
 
