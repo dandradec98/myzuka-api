291b537c6150b59c035b14d959bb3d43e7803e47
dandradec <dandradec98@gmail.com>
2019-02-06 13:47:24 -0500

    adding cors module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const rp = require("request-promise");
 const utf8 = require("utf8");
 const getJSON = require("get-json");
+const cors = require("cors");
 
 const port = process.env.PORT || 8080;
 
@@ -251,13 +252,7 @@ app.get("/Artist/:id/:name/Albums", (req, res) => {
         });
 });
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use(cors());
 
 console.log(`Myzuka API running on port ${port}`);
 
